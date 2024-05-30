68177ed2869f24529a99abfe790d3b4e0e4e4d22
patyzhy <163975762+patyzhy@users.noreply.github.com>
2024-05-30 19:58:23 +0800

    initial commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAGLNZf-BMK6M6dvK_J2k3sg5KTMaTC8C4",
+  authDomain: "crud-681b7.firebaseapp.com",
+  databaseURL: "https://crud-681b7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-681b7",
+  storageBucket: "crud-681b7.appspot.com",
+  messagingSenderId: "329018044208",
+  appId: "1:329018044208:web:67a49d010a773b9404e2f5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
